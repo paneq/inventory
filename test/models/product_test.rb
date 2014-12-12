d9ec747dc4607eddff1741e9cc05d14f0c4bfa1c
Robert Pankowecki <robert.pankowecki@gmail.com>
2014-12-12 21:40:49 +0100

    How about we don't lose history? Almost event sourcing ;)

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,52 +4,53 @@ class ProductTest < ActiveSupport::TestCase
 
   class Inventory
     def initialize
-      @available_quantity = Hash.new(0)
-      @reserved_quantity  = Hash.new(0)
-      @sold_quantity      = Hash.new(0)
+      @available_quantity = Hash.new{|hash, key| hash[key] = [0] }
+      @reserved_quantity  = Hash.new{|hash, key| hash[key] = [0] }
+      @sold_quantity      = Hash.new{|hash, key| hash[key] = [0] }
     end
 
     def register_product(identifier, available_quantity)
       #Product.create!(name: identifier)
-      @available_quantity[identifier] = available_quantity
+      @available_quantity[identifier] << available_quantity
     end
 
     def available_quantity(identifier)
-      @available_quantity[identifier] - @reserved_quantity[identifier] - @sold_quantity[identifier]
+      @available_quantity[identifier].sum - @reserved_quantity[identifier].sum - @sold_quantity[identifier].sum
     end
 
     def change_quantity(identifier, qty)
-      raise StandardError, "quantity too low" if qty - @reserved_quantity[identifier] - @sold_quantity[identifier] < 0
-      @available_quantity[identifier] = qty
+      raise StandardError, "quantity too low" if qty - @reserved_quantity[identifier].sum - @sold_quantity[identifier].sum < 0
+      @available_quantity[identifier] << -@available_quantity[identifier].last
+      @available_quantity[identifier] << qty
     end
 
     def reserved_quantity(identifier)
-      @reserved_quantity[identifier]
+      @reserved_quantity[identifier].sum
     end
 
     def sold_quantity(identifier)
-      @sold_quantity[identifier]
+      @sold_quantity[identifier].sum
     end
 
     def reserve_product(identifier, qty)
       raise StandardError, "quantity too big" if available_quantity(identifier) - qty < 0
-      @reserved_quantity[identifier]  += qty
+      @reserved_quantity[identifier]  << qty
     end
 
     def sell_product(identifier, qty)
       raise StandardError, "quantity too big" if reserved_quantity(identifier) - qty < 0
-      @reserved_quantity[identifier] -= qty
-      @sold_quantity[identifier]     += qty
+      @reserved_quantity[identifier] << -qty
+      @sold_quantity[identifier]     << qty
     end
 
     def expire_product(identifier, qty)
       raise StandardError, "quantity too big" if qty > reserved_quantity(identifier)
-      @reserved_quantity[identifier] -= qty
+      @reserved_quantity[identifier] << -qty
     end
 
     def refund_product(identifier, qty)
       raise StandardError, "quantity too big" if qty > sold_quantity(identifier)
-      @sold_quantity[identifier] -= qty
+      @sold_quantity[identifier] << -qty
     end
   end
 
