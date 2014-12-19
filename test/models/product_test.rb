102e8399cfce525d65962bac93a8714961f55fc2
Robert Pankowecki <robert.pankowecki@gmail.com>
2014-12-19 10:45:29 +0100

    initial available quantity for product

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -7,10 +7,21 @@ class InventoryTest < ActiveSupport::TestCase
 
     end
 
+    def available_quantity(identifier)
+      10
+    end
+
   end
 
   test "can add product with initial available quantity" do
     inventory = Inventory.new
     inventory.register_product("WROCLOVE2014", 10)
+  end
+
+  test "can get initial quantity" do
+    inventory = Inventory.new
+    inventory.register_product("WROCLOVE2014", 10)
+    qty = inventory.available_quantity("WROCLOVE2014")
+    assert_equal 10, qty
   end
 end
