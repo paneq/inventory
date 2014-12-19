a581daaea924ff34584c63616666b4c1fa45d11d
Robert Pankowecki <robert.pankowecki@gmail.com>
2014-12-19 10:43:09 +0100

    register_product

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,13 +1,16 @@
 require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase
+class InventoryTest < ActiveSupport::TestCase
 
   class Inventory
+    def register_product(identifier, initial_quantity)
+
+    end
 
   end
 
-  test "can add product" do
+  test "can add product with initial available quantity" do
     inventory = Inventory.new
-    inventory.register_product("wroc_love.rb 2014 ticket")
+    inventory.register_product("WROCLOVE2014", 10)
   end
 end
